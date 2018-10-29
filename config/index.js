1e824f36d334fcd7fed427a9ddce7e95b41ffaf3
Bruno Gusmão <brunohgv2@gmail.com>
2018-10-29 11:45:11 -0300

    mudança no public-path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/GUIA-DO-COMMITEIRO',
+    assetsPublicPath: '/guia-do-commiteiro',
 
     /**
      * Source Maps
